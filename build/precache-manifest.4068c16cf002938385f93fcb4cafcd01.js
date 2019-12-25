c16dff0c7bdb07a278bb57f24e861d4200a18c61
98o718 <kalyakin.maks@mail.ru>
2019-12-25 21:33:06 +0300

    Build new version

---
+++ build/precache-manifest.4068c16cf002938385f93fcb4cafcd01.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8832cbc84e4adfa8fe85d21dacc70867",
+    "url": "/mlp-front/26767caa5c4ccaa857e3.worker.js"
+  },
+  {
+    "revision": "d535950438739d54c887503ba0c5991c",
+    "url": "/mlp-front/26767caa5c4ccaa857e3.worker.js.LICENSE"
+  },
+  {
+    "revision": "45f1c42ad251e9e4200a0c737dfed44c",
+    "url": "/mlp-front/index.html"
+  },
+  {
+    "revision": "17d3d604a4358f632b5c",
+    "url": "/mlp-front/static/css/2.f44e27ca.chunk.css"
+  },
+  {
+    "revision": "17d3d604a4358f632b5c",
+    "url": "/mlp-front/static/js/2.93219e62.chunk.js"
+  },
+  {
+    "revision": "471ad2ad75b77d908147e3dc61371ff6",
+    "url": "/mlp-front/static/js/2.93219e62.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c6ec0fdee20c076ee6fe",
+    "url": "/mlp-front/static/js/main.fd64bad4.chunk.js"
+  },
+  {
+    "revision": "d02a98c7c799139fab82",
+    "url": "/mlp-front/static/js/runtime-main.e7a7b60a.js"
+  }
+]);
