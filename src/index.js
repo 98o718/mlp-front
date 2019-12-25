0554a9231ac96a594876a2f6ea6898e4e321d3d5
98o718 <kalyakin.maks@mail.ru>
2019-12-26 00:26:59 +0300

    Build new version

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { App } from './components/app'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'rc-input-number/assets/index.css'
-
-ReactDOM.render(<App />, document.getElementById('root'))
